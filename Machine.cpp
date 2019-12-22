fc126f1232827a41646aef735e2ddf5101a284b5
Krasimir Shontov <krasi_964@abv.bg>
2019-12-22 22:18:47 +0200

    Add printing to the console

--- Machine.cpp
+++ Machine.cpp
@@ -24,7 +24,7 @@ void Machine::save(ostream& os) {
 	os << "Tape: " << (string)tape << '\n';
 
 	os << "States: " << '\n';
-	states.printStates(os);
+	states.printStates(os, false);
 
 	os << "Transitions: " << '\n';
 	states.printTransitions(os);
@@ -37,6 +37,33 @@ string Machine::executeAndGetTape() {
 	return (string)tape;
 }
 
+void Machine::print() {
+	cout << endl;
+	cout << YELLOW_TEXT << "------------------------------------------------\n";
+	cout << YELLOW_TEXT << "\t\t| Machine <" << id << "> |" << RESET_COLORING << endl;
+	cout << YELLOW_TEXT << "------------------------------------------------\n";
+
+	cout << YELLOW_TEXT << "Blank symbol: " << tape.getBlank() << endl << RESET_COLORING;
+
+	cout << YELLOW_TEXT << "Sigma: " << RESET_COLORING;
+	symbols.printSymbols(cout, symbols.sigma);
+
+	cout << YELLOW_TEXT << "Gamma: " << RESET_COLORING;
+	symbols.printSymbols(cout, symbols.gamma);
+
+	cout << YELLOW_TEXT << "Tape: " << RESET_COLORING;
+	cout << (string)tape << endl;
+
+	cout << YELLOW_TEXT << "States: " << RESET_COLORING << endl;
+	states.printStates(cout, true);
+	
+	cout << YELLOW_TEXT << "Transitions: " << RESET_COLORING << endl;
+	states.printTransitions(cout);
+	cout << YELLOW_TEXT << "------------------------------------------------\n" << RESET_COLORING;
+	cout << endl;
+
+}
+
 void MachineSymbols::printSymbols(ostream& os, symbol_set printable) {
 	for (auto it = printable.begin(); it != printable.end(); it++) {
 		os << *it;
@@ -45,4 +72,17 @@ void MachineSymbols::printSymbols(ostream& os, symbol_set printable) {
 		}
 	}
 	os << '\n';
+}
+
+MachineSymbols& MachineSymbols::compose(MachineSymbols& other) {
+	if (other.blank_symbol != this->blank_symbol) {
+		throw invalid_argument("Attempt to compose machines with non-matching blank symbols");
+	}
+	symbol_set new_sigma = *(new symbol_set(sigma));
+	new_sigma.insert(other.sigma.begin(), other.sigma.end());
+
+	symbol_set new_gamma = *(new symbol_set(gamma));
+	new_gamma.insert(other.gamma.begin(), other.gamma.end());
+
+	return *(new MachineSymbols(new_sigma, new_gamma, blank_symbol));
 }
--- Machine.h
+++ Machine.h
@@ -3,14 +3,39 @@
 #include <set>
 #include "Tape.h"
 #include "StatesUtil.h"
-#include <ostream>
+#include <iostream>
+#include <algorithm>
 
+using std::cout;
+using std::endl;
 using std::set;
 using std::ostream;
+using std::invalid_argument;
+using std::set_union;
+
 class Machine;
 
-
 typedef set<char> symbol_set;
+
+const string BLACK_TEXT = "\x1B[30m";
+const string RED_TEXT = "\x1B[31m";
+const string GREEN_TEXT = "\x1B[32m";
+const string YELLOW_TEXT = "\x1B[33m";
+const string BLUE_TEXT = "\x1B[34m";
+const string MAGENTA_TEXT = "\x1B[35m";
+const string CYAN_TEXT = "\x1B[36m";
+const string WHITE_TEXT = "\x1B[37m";
+const string BRIGHT_BLACK_TEXT = "\x1B[90m";
+const string BRIGHT_RED_TEXT = "\x1B[91m";
+const string BRIGHT_GREEN_TEXT = "\x1B[92m";
+const string BRIGHT_YELLOW_TEXT = "\x1B[93m";
+const string BRIGHT_BLUE_TEXT = "\x1B[94m";
+const string BRIGHT_MAGENTA_TEXT = "\x1B[95m";
+const string BRIGHT_CYAN_TEXT = "\x1B[96m";
+const string BRIGHT_WHITE_TEXT = "\x1B[97m";
+const string RESET_COLORING = "\033[0m";
+
+static unsigned produced_machines = 0;
 
 struct MachineSymbols {
 	symbol_set sigma;
@@ -19,20 +44,24 @@ struct MachineSymbols {
 
 	MachineSymbols(symbol_set sigma = symbol_set(), symbol_set gamma = symbol_set(), char blank_symbol = ' ') : sigma(sigma), gamma(gamma), blank_symbol(blank_symbol) {};
 	void printSymbols(ostream& os, symbol_set printable);
-
+	MachineSymbols& compose(MachineSymbols& other);
 };
 
 class Machine {
 	friend class MachineBuilder;
 public:
 
-	Machine(MachineSymbols symbols = MachineSymbols(), StatesUtil states = StatesUtil(), Tape tape = Tape()) : symbols(symbols), states(states), tape(tape) {};
+	Machine(MachineSymbols symbols = MachineSymbols(), StatesUtil states = StatesUtil(), Tape tape = Tape()) : id(produced_machines++), symbols(symbols), states(states), tape(tape) {};
 
 	bool execute();
 	void save(ostream& os);
 	string executeAndGetTape();
+	void print();
+
+	Machine& compose(const Machine& other);
 
 private:
+	unsigned id;
 	Tape tape;
 	MachineSymbols symbols;
 	StatesUtil states;
